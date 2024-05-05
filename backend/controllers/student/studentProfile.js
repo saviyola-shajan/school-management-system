bd0e42a42c215c28eab65564c07c937e2b500143
saviyola-shajan <saviyolashajan69@gmail.com>
2024-05-05 22:47:17 +0530

    started user profile

--- backend/controllers/student/studentProfile.js
+++ backend/controllers/student/studentProfile.js
@@ -1,2 +1,11 @@
 import asyncHandler from "express-async-handler";
 import Student from "../../modles/student/studentModel.js";
+
+export const userDetails=asyncHandler(async(req,res)=>{
+    try{
+        const studentId=req.body
+const userDetails=await Student.findOne({studentId})
+    }catch(error){
+        throw new Error(error)
+    }
+})
