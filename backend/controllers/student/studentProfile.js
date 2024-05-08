9b0b163720d232c68853217929d20fc11be24204
saviyola-shajan <saviyolashajan69@gmail.com>
2024-05-08 16:52:06 +0530

    started frontend admin student

--- backend/controllers/student/studentProfile.js
+++ backend/controllers/student/studentProfile.js
@@ -1,10 +1,12 @@
 import asyncHandler from "express-async-handler";
 import Student from "../../modles/student/studentModel.js";
 
-export const userDetails=asyncHandler(async(req,res)=>{
+// get student details
+export const studentDetails=asyncHandler(async(req,res)=>{
     try{
-        const studentId=req.body
+        const studentId=req.params._id
 const userDetails=await Student.findOne({studentId})
+res.status(201).json({userDetails,message:"user deatils fetched"})
     }catch(error){
         throw new Error(error)
     }
--- backend/controllers/teacher/teacherProfile.js
+++ backend/controllers/teacher/teacherProfile.js
@@ -1,2 +1,13 @@
 import asyncHandler from "express-async-handler";
 import Teacher from "../../modles/teacher/teacherModel.js";
+
+// get teacher details
+export const teacherDetails=asyncHandler(async(req,res)=>{
+    try{
+        const teacherId=req.params._id
+const teacherDetails=Teacher.findOne({teacherId})
+res.status(201).json({teacherDetails,message:"teacher details fetched"})
+    }catch(error){
+        throw new Error(error)
+    }
+})
